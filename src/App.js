1ed0ab51d6100de9bf3d09680010de6cb36b076d
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-08 09:49:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './component/Header'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <ul className="list">
+        <li className="note">
+          <h4 className="title">안녕하세욧</h4>
+          <p>안녕하세요 1111</p>
+          <div className="bottom">
+            <div className="date">2022-10-20</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h4 className="title">안녕하세욧</h4>
+          <p>안녕하세요 1111</p>
+          <div className="bottom">
+            <div className="date">2022-10-20</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h4 className="title">안녕하세욧</h4>
+          <p>안녕하세요 1111</p>
+          <div className="bottom">
+            <div className="date">2022-10-20</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h4 className="title">안녕하세욧</h4>
+          <p>안녕하세요 1111</p>
+          <div className="bottom">
+            <div className="date">2022-10-20</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h4 className="title">안녕하세욧</h4>
+          <p>안녕하세요 1111</p>
+          <div className="bottom">
+            <div className="date">2022-10-20</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;

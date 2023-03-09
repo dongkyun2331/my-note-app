030f0818cff88699301753bcde3ff285e1d2e0bd
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-09 11:14:23 +0900

    a

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,47 +1,48 @@
 import { useRef } from 'react'
-import moment from 'moment/moment'
-import 'moment/locale/ko'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-export default function Write() {
-  const navigate = useNavigate()
+export default function Edit() {
   const titleRef = useRef()
   const detailRef = useRef()
+  const note = useLocation().state
+  const navigte = useNavigate()
 
-  function onWrite(e) {
+  function onEdit(e) {
     e.preventDefault()
-    console.log(titleRef.current.value)
-    console.log(detailRef.current.value)
-
-    fetch('http://localhost:3001/notes', {
-      method: 'POST',
+    fetch(`http://localhost:3001/notes/${note.id}`, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
+        ...note,
         title: titleRef.current.value,
         detail: detailRef.current.value,
-        date: moment().format('YYYY년 MMMM Do, h:mm:ss '),
       }),
     }).then((res) => {
       if (res.ok) {
-        navigate('/')
+        navigte('/')
       }
     })
   }
 
   function goBack(e) {
     e.preventDefault()
-    navigate(-1)
+    navigte(-1)
   }
 
   return (
     <div className="container">
-      <h2>노트 작성</h2>
+      <h2>노트 수정</h2>
       <form>
         <div>
           <label>제목</label>
-          <input type="text" placeholder="제목" ref={titleRef} />
+          <input
+            type="text"
+            placeholder="제목"
+            ref={titleRef}
+            defaultValue={note.title}
+          />
         </div>
         <div>
           <label>내용</label>
@@ -50,11 +51,12 @@ export default function Write() {
             rows="10"
             placeholder="내용"
             ref={detailRef}
+            defaultValue={note.detail}
           ></textarea>
         </div>
         <div>
           <button onClick={goBack}>취소하기</button>
-          <button onClick={onWrite}>작성하기</button>
+          <button onClick={onEdit}>작성하기</button>
         </div>
       </form>
     </div>

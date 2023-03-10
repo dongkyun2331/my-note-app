7b3fb42ef1e2195b742e4bf7da513c1a985286c7
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-10 11:08:39 +0900

    a

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,12 +3,14 @@ import { MdMode, MdDelete } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import 'moment/locale/ko'
+import { db } from '../firebase'
+import { doc, deleteDoc } from 'firebase/firestore'
 
 export default function Note({ note }) {
   const date = moment(note.date.toDate()).format('YYYY-MM-D h:mm')
 
-  function onDelete() {
-    //
+  async function onDelete() {
+    await deleteDoc(doc(db, 'notes', 'note.id'))
   }
 
   return (
